e39c3ba7c1d57eba4ca572268773ac5846fabda8
johndolac <johndolacjr@gmail.com>
2020-10-15 15:18:41 -0600

    corrected color change features

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ for (var i=0; i < hour.length; i++) {
         `<div id="hour-${hour[i]}" class="row time-block">
             <div class="col-md-1 hour"> ${displayHour[i]}</div>
             <textarea class="col-md-10 description"/>
-            <button class="button saveBtn col-md-1">Save</button>
+            <button class="button saveBtn col-md-1"><i class="fas fa-save"></i></button>
         </div>
         `
     )
@@ -21,11 +21,12 @@ for (var i=0; i < hour.length; i++) {
         $(`#hour-${hour[i]}`).removeClass("past"); 
         $(`#hour-${hour[i]}`).addClass("present"); 
     } 
-    else {
-        $(this).removeClass("past"); 
-        $(this).removeClass("present"); 
-        $(this).addClass("future");
-    }    
+
+    else {(moment().format("H") < hour[i]);
+        $(`#hour-${hour[i]}`).removeClass("past"); 
+        $(`#hour-${hour[i]}`).removeClass("present"); 
+        $(`#hour-${hour[i]}`).addClass("future"); 
+        } 
 }; 
 
 $(".saveBtn").on("click", function(){
@@ -37,55 +38,3 @@ $(".saveBtn").on("click", function(){
 for (var i=9; i <= 17; i++){
     $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`))
 }
-
-//class and if statements 
-
-
-
-
-
-
-
-
-
-
-
-// // // selectors
-// // const todoInput = document.querySelector(".todo-input");
-// // const todoButton = document.querySelector(".todo-button");
-// // const todoList = document.querySelector(".todo-list");
-
-
-// // //Listeners
-// // todoButton.addEventListener("click", addTodo);
-
-
-// // //functions
-// // fuction addTodo (Event) {
-// //     Event.preventDefault(); 
-
-// // //Create ToDo Div 
-// // const todoDiv = document.createElement("div");
-// // todoDiv.classList.add("todo");
-
-// // //create LI
-// // const newTodo = document.createElement("li");
-// // newTodo.innerText = "hey"; 
-// // newTodo.classList.add("todo-item"); 
-// // todoDiv.appendChild(newTodo);
-
-// // //Save Button
-// // const saveButton = document.createEleemnt("button");
-// // saveButton.innerHTML = "<i class="fas fa-save"></i>"
-// // saveButton.classList.add("save-btn");
-// // todoDiv.appendChild(saveButton);
-
-// // //Trash Button 
-// // const trashButton = document.createEleemnt("button");
-// // trashButton.innerHTML = "<i class="fas fa-trash-alt"></i>"; 
-// // trashButton.classList.add("trash-btn");
-// // todoDiv.appendChild(trashButton);
-
-// // //Append to list
-// // todoList.appendChild(todoDiv);
-// }
